56dd2b2bd9316bbbaf95e7300005e109b2155757
pavithrasekar24 <spavithracse8@gmail.com>
2022-05-09 21:13:59 +0530

    1 file modified

--- src/Component/Title/Title.jsx
+++ src/Component/Title/Title.jsx
@@ -9,7 +9,16 @@ export default function Title() {
     </div>
   );
 }
+
 // useNavigate navigate url , state:{} -data
 // useLocation -fetch state object (history object)
 // useParam url/id -> param.id
-//subrouting -outlet
+// subrouting -outlet
+// task---
+// step2 //login -> []   {id username, pwd , role} -10json -validate - some()//
+
+// step1
+// dropdown -> admin/agent remove
+// admin header + content
+// agent sidebar +content
+//logout
